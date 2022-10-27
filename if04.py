0324513eaa57c17eff58876e96a6078abea3b283
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-10-27 21:52:09 +0500

    if04.py

--- if04.py
+++ if04.py
@@ -8,4 +8,24 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    x1 = int(a)
+    x2 = int(b) 
+    x3 = int(c)
+    if x1>0 and x2>0 and x3>0:
+        return 3
+    if x1>0 and x2>0 and x3<0:
+        return 2
+    if x1>0 and x3>0 and x2<0:
+        return 2
+    if x3>0 and x2>0 and x1<0:
+        return 2
+    if x1>0 and x2<0 and x3<0:
+        return 1 
+    if x1<0 and x2>0 and x3<0:
+        return 1
+    if x1<0 and x2<0 and x3>0:
+        return 1
+    if x1<0 and x2<0 and x3<0:
+        return 0
+
+print(main(2,-4,-8))
